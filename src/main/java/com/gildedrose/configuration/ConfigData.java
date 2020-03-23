a6ed33ca949eb8c690fc90ebe93ab3307b40b1af
Andrejus <an.kuznecovas@gmail.com>
2020-03-23 10:55:40 +0200

    Add ConfigData class to store configurable values

---
+++ src/main/java/com/gildedrose/configuration/ConfigData.java
@@ -0,0 +1,14 @@
+package com.gildedrose.configuration;
+
+public class ConfigData {
+
+  private ConfigData() {
+  } //Utility classes should not have public constructors
+
+  public static final int NORMAL_SELLIN_VALUE = 1;
+  public static final int NON_EXPIRED_ITEM_DECAY_VALUE = 1;
+  public static final int EXPIRED_ITEM_DECAY_VALUE = 2;
+  public static final int CONJURED_ITEM_DECAY_VALUE = 2;
+  public static final int ITEM_QUALITY_MAX_VALUE = 50;
+  public static final int ITEM_QUALITY_MIN_VALUE = 0;
+}
