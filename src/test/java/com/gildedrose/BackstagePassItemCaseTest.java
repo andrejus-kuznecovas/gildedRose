c85916f24c4ad223592377912ad8c0a57097c77d
Andrejus <an.kuznecovas@gmail.com>
2020-03-23 10:49:13 +0200

    Add Backstage Pass Item Case Tests

---
+++ src/test/java/com/gildedrose/BackstagePassItemCaseTest.java
@@ -0,0 +1,48 @@
+package com.gildedrose;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class BackstagePassItemCaseTest {
+
+  @Test
+  public void backstage_pass_quality_increases_slow_long_before_concert() {
+    Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 20, 5)};
+    GildedRose gildedRose = new GildedRose(items);
+    gildedRose.updateQuality();
+
+    assertEquals(19, gildedRose.items[0].sellIn);
+    assertEquals(6, gildedRose.items[0].quality);
+  }
+
+  @Test
+  public void backstage_pass_quality_increases_medium_before_concert() {
+    Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 7, 5)};
+    GildedRose gildedRose = new GildedRose(items);
+    gildedRose.updateQuality();
+
+    assertEquals(6, gildedRose.items[0].sellIn);
+    assertEquals(7, gildedRose.items[0].quality);
+  }
+
+  @Test
+  public void backstage_pass_quality_increases_quick_just_before_concert() {
+    Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 5, 5)};
+    GildedRose gildedRose = new GildedRose(items);
+    gildedRose.updateQuality();
+
+    assertEquals(4, gildedRose.items[0].sellIn);
+    assertEquals(8, gildedRose.items[0].quality);
+  }
+
+  @Test
+  public void backstage_pass_quality_is_zero_after_concert() {
+    Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 0, 5)};
+    GildedRose gildedRose = new GildedRose(items);
+    gildedRose.updateQuality();
+
+    assertEquals(-1, gildedRose.items[0].sellIn);
+    assertEquals(0, gildedRose.items[0].quality);
+  }
+}
