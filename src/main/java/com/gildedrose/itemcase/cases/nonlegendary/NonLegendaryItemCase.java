5585ea7f77f1f763a5adf065660ab25ef9143142
Andrejus <an.kuznecovas@gmail.com>
2020-03-23 11:05:43 +0200

    Implement NonLegendaryItemCase class methods

--- src/main/java/com/gildedrose/itemcase/cases/nonlegendary/NonLegendaryItemCase.java
+++ src/main/java/com/gildedrose/itemcase/cases/nonlegendary/NonLegendaryItemCase.java
@@ -1,7 +1,29 @@
 package com.gildedrose.itemcase.cases.nonlegendary;
 
+import static com.gildedrose.configuration.ConfigData.EXPIRED_ITEM_DECAY_VALUE;
+import static com.gildedrose.configuration.ConfigData.ITEM_QUALITY_MAX_VALUE;
+import static com.gildedrose.configuration.ConfigData.ITEM_QUALITY_MIN_VALUE;
+import static com.gildedrose.configuration.ConfigData.NON_EXPIRED_ITEM_DECAY_VALUE;
+import static com.gildedrose.configuration.ConfigData.NORMAL_SELLIN_VALUE;
+
+import com.gildedrose.Item;
 import com.gildedrose.itemcase.ItemCase;
 
 public abstract class NonLegendaryItemCase implements ItemCase {
 
+  @Override
+  public void updateItemQualityAndSellIn(Item item) {
+    item.quality = setQualityToNotExceedMinMaxValues(getUpdatedQuality(item));
+    item.sellIn -= NORMAL_SELLIN_VALUE;
+  }
+
+  public int setQualityToNotExceedMinMaxValues(int quality) {
+    return Math.min(Math.max(quality, ITEM_QUALITY_MIN_VALUE), ITEM_QUALITY_MAX_VALUE);
+  }
+
+  public int getQualityChange(Item item) {
+    return item.sellIn > 0 ? NON_EXPIRED_ITEM_DECAY_VALUE : EXPIRED_ITEM_DECAY_VALUE;
+  }
+
+  public abstract int getUpdatedQuality(Item item);
 }
