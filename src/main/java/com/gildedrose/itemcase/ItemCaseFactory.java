6e11a9639bf78ad6548113ef492676917a7ed0ad
Andrejus <an.kuznecovas@gmail.com>
2020-03-23 11:17:37 +0200

    Implement ItemCaseFactory.getItemCaseForItemName method

--- src/main/java/com/gildedrose/itemcase/ItemCaseFactory.java
+++ src/main/java/com/gildedrose/itemcase/ItemCaseFactory.java
@@ -1,8 +1,25 @@
 package com.gildedrose.itemcase;
 
+import com.gildedrose.configuration.ConfigData;
+import com.gildedrose.itemcase.cases.nonlegendary.NormalItemCase;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Optional;
+
 public class ItemCaseFactory {
 
+  private ItemCaseFactory() {
+  } //Utility classes should not have public constructors
+
+  private static final Map<String, ItemCase> itemCaseMap = ConfigData.getItemCaseMap();
+
   public static ItemCase getItemCaseForItemName(String name) {
-    return null;
+    Optional<ItemCase> itemCaseOptional = itemCaseMap.entrySet()
+        .stream()
+        .filter(entry -> name.startsWith(entry.getKey()))
+        .findFirst()
+        .map(Entry::getValue);
+
+    return itemCaseOptional.isPresent() ? itemCaseOptional.get() : new NormalItemCase();
   }
 }
