4e0abe4d60f3d6ecea942da0b2e63b98563c4149
Andrejus <an.kuznecovas@gmail.com>
2020-03-23 11:03:31 +0200

    Add non legendary item cases package with empty classes

---
+++ src/main/java/com/gildedrose/itemcase/cases/nonlegendary/NonLegendaryItemCase.java
@@ -0,0 +1,7 @@
+package com.gildedrose.itemcase.cases.nonlegendary;
+
+import com.gildedrose.itemcase.ItemCase;
+
+public abstract class NonLegendaryItemCase implements ItemCase {
+
+}
---
+++ src/main/java/com/gildedrose/itemcase/cases/nonlegendary/NormalItemCase.java
@@ -0,0 +1,11 @@
+package com.gildedrose.itemcase.cases.nonlegendary;
+
+import com.gildedrose.Item;
+
+public class NormalItemCase extends NonLegendaryItemCase {
+
+  @Override
+  public void updateItemQualityAndSellIn(Item item) {
+
+  }
+}
